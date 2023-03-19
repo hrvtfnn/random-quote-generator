2616067b6acbe7a49018c8ee476399c658fdd2d5
hrvtfnn <hrvtfnn@gmail.com>
2023-03-19 15:40:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,84 @@
 const quoteText = document.querySelector(".quote"),
-quoteBtn = document.querySelector(".new"),
-authorName = document.querySelector(".name"),
-copyBtn = document.querySelector(".copy"),
-fbBtn = document.querySelector(".facebook")
-const wikiBtn = document.querySelector(".read")
+  quoteBtn = document.querySelector(".new"),
+  authorName = document.querySelector(".name"),
+  copyBtn = document.querySelector(".copy"),
+  fbBtn = document.querySelector(".facebook");
+const wikiBtn = document.querySelector(".read");
 
 // Random Quotes
-function randomQuote(){
-    quoteBtn.classList.add("loading");
-    quoteBtn.innerText = "Loading Quote...";
-    fetch("https://api.quotable.io/random?minLength=100&maxLength=140")
-    .then(response => response.json())
-    .then(result => {
-        quoteText.innerText = result.content;
-        authorName.innerText = result.author;
-        quoteBtn.classList.remove("loading");
-        quoteBtn.innerText = "New Quote";
+function randomQuote() {
+  quoteBtn.classList.add("loading");
+  quoteBtn.innerText = "Loading Quote...";
+  fetch("https://api.quotable.io/random?minLength=100&maxLength=140")
+    .then((response) => response.json())
+    .then((result) => {
+      quoteText.innerText = result.content;
+      authorName.innerText = result.author;
+      quoteBtn.classList.remove("loading");
+      quoteBtn.innerText = "New Quote";
 
-        const button = document.querySelector('.read');
-
-})}   
+      const button = document.querySelector(".read");
+    });
+}
 
 // New Quote button
-if (quoteBtn){
-quoteBtn.addEventListener("click", randomQuote);
+if (quoteBtn) {
+  quoteBtn.addEventListener("click", randomQuote);
 }
 
 // Copy button
-copyBtn.addEventListener("click", ()=>{
-    navigator.clipboard.writeText(quoteText.innerText);
-    copyBtn.classList.add("active");
-    setTimeout(function(){
-        copyBtn.classList.remove("active");
-    },1000)
-})
+copyBtn.addEventListener("click", () => {
+  navigator.clipboard.writeText(quoteText.innerText);
+  copyBtn.classList.add("active");
+  setTimeout(function () {
+    copyBtn.classList.remove("active");
+  }, 1000);
+});
 
 // Facebook button
-fbBtn.addEventListener("click", ()=>{
-    let fbUrl = `https://facebook.com/share.php=${quoteText.innerText}`;
-    window.open(fbUrl, "_blank");
+fbBtn.addEventListener("click", () => {
+  let fbUrl = `https://facebook.com/share.php=${quoteText.innerText}`;
+  window.open(fbUrl, "_blank");
 });
 
 // Read more on Wikipedia
-        
-wikiBtn.addEventListener('click', () => {
+wikiBtn.addEventListener("click", () => {
   fetch("https://api.quotable.io/random?minLength=100&maxLength=140")
-    .then(response => response.json())
-    .then(result => {
-      let authorName = document.querySelector(".name")
-      let searchUrl = "https://wikipedia.org/wiki/" + (authorName.innerText);
-      window.open(searchUrl, '_blank');
+    .then((response) => response.json())
+    .then((result) => {
+      let authorName = document.querySelector(".name");
+      let searchUrl = "https://wikipedia.org/wiki/" + authorName.innerText;
+      window.open(searchUrl, "_blank");
     })
-    .catch(error => console.error(error));
-}); 
+    .catch((error) => console.error(error));
+});
+
+// Color Switcher
+
+const colorThemes = document.querySelectorAll("[name=theme]");
+// store theme
+const storeTheme = function (theme) {
+  localStorage.setItem("theme", theme);
+};
+
+// set theme when visitor returns
+const setTheme = function () {
+  const activeTheme = localStorage.getItem("theme");
+  colorThemes.forEach((themeOption) => {
+    if (themeOption.id === activeTheme) {
+      themeOption.checked = true;
+    }
+  });
+  // fallback for no :has() support
+  document.documentElement.className = activeTheme;
+};
+
+colorThemes.forEach((themeOption) => {
+  themeOption.addEventListener("click", () => {
+    storeTheme(themeOption.id);
+    // fallback for no :has() support
+    document.documentElement.className = themeOption.id;
+  });
+});
+
+document.onload = setTheme();
